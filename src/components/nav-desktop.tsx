f7a11a51d2932c5e28199f2e3c9729c2fb694d06
Cecilia Catanzano <cecilia.catanzano@gmail.com>
2023-12-30 21:51:05 +0100

    started

--- src/components/nav-desktop.tsx
+++ src/components/nav-desktop.tsx
@@ -1,5 +1,20 @@
 import { routes } from "../routes";
 
 export const NavDesktop = () => {
-  return <div>desktop</div>;
+  return (
+    <div>
+      <ul className="hidden lg:flex lg:items-center gap-5 text-sm"> desktop</ul>
+      {routes.map((route) => {
+        const { Icon, href, title } = route;
+        return (
+          <li key={title}>
+            <a href={href}>
+              {" "}
+              <Icon /> {title}
+            </a>
+          </li>
+        );
+      })}
+    </div>
+  );
 };
